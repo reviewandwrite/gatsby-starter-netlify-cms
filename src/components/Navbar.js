6323b6f88d5bcf5b659bf910ef8656f7f90392e4
Arindam Nath <randykethcena@gmail.com>
2022-08-31 15:58:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div className="container">
         <div className="navbar-brand">
           <Link to="/" className="navbar-item" title="Logo">
-            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+            <img src={logo} alt="Kaldi" style={{ width: "88px", max-height: "4rem" }} />
           </Link>
           {/* Hamburger menu */}
           <button
