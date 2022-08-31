e9e0e970dffd8a2172892dacf8330d5f29075e23
Arindam Nath <randykethcena@gmail.com>
2022-08-31 15:59:42 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,21 +53,14 @@ const Footer = () => {
                   </ul>
                 </section>
               </div>
-              <!-- <div className="column is-4 social">-->
-                <!-- <a title="facebook" href="https://facebook.com">-->
-                  <!-- <img-->
-                   <!--  src={facebook}-->
-                   <!--  alt="Facebook"-->
-                    <!-- style={{ width: "1em", height: "1em" }}-->
-                 <!--  />-->
-                <!-- </a>-->
-                <!-- <a title="twitter" href="https://twitter.com">-->
-                <!--   <img-->
-                  <!--   className="fas fa-lg"-->
-                   <!--  src={twitter}-->
-                   <!--  alt="Twitter"-->
-                   <!--  style={{ width: "1em", height: "1em" }}-->
-                 <!--  />-->
+              <div className="column is-4 social">
+                <a title="twitter" href="https://twitter.com">
+                 <img
+                  className="fas fa-lg"
+                   src={twitter}
+                   alt="Twitter"
+                   style={{ width: "1em", height: "1em" }}
+                  />
                 </a>
                 <a title="instagram" href="https://www.instagram.com/reviewandwrite/">
                   <img
