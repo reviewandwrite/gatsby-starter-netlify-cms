3ee86fcc685e1ea48badd87c44cd74c091ea0d55
Arindam Nath <randykethcena@gmail.com>
2022-08-31 15:41:44 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,33 +53,26 @@ const Footer = () => {
                   </ul>
                 </section>
               </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: "1em", height: "1em" }}
-                  />
+              <!-- <div className="column is-4 social">-->
+                <!-- <a title="facebook" href="https://facebook.com">
+                  <!-- <img
+                   <!--  src={facebook}
+                   <!--  alt="Facebook"
+                    <!-- style={{ width: "1em", height: "1em" }}
+                 <!--  />
+                <!-- </a>
+                <!-- <a title="twitter" href="https://twitter.com">
+                <!--   <img
+                  <!--   className="fas fa-lg"
+                   <!--  src={twitter}
+                   <!--  alt="Twitter"
+                   <!--  style={{ width: "1em", height: "1em" }}
+                 <!--  />-->
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/reviewandwrite/">
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
